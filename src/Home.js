6bfaea2efcf13b7edac6fb4d41ffadaed77ba79a
Egemen <egemenwasd@gmail.com>
2022-06-07 00:48:51 +0300

    fetch the data and set up hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,29 @@
+import { useEffect, useState} from "react";
+import Notifications from "./Notifications";
+import useFetch from "./useFetch";
+
 const Home = () => {
+	
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		fetch("./data/data.json")
+		.then((response) => response.json())
+		.then((json) => {
+			console.log(json);
+			setData(json.notifications)
+		});
+	});
+
+
+    // fetch("./data/data.json")
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     console.log(json);
+    //   });
+
 	return (
 		<div className="container">
-			home
+			<Notifications NotifData={data} />
 		</div>
 	);
 }
