bdc70a81a679a39dc61e3d53907ad4f4a6712e57
Egemen <egemenwasd@gmail.com>
2022-06-06 22:44:04 +0300

    navbar and body color added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,16 @@
 const Navbar = () => {
   return ( 
-    <div className="nabar">
-      nav
-    </div>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="container">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          <h3 className="is-size-1 nav-icon">inyova</h3>
+        </a>
+      </div>
+      <div className="navbar-end">
+        </div>
+      </div>
+    </nav>
    );
 }
  
