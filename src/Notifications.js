efc554d5e2432849e31b9011b6121b3de20aa72e
Egemen <egemenwasd@gmail.com>
2022-06-07 15:25:54 +0300

    toggle function added to descripton div + comments

--- src/Notifications.js
+++ src/Notifications.js
@@ -1,7 +1,18 @@
+
+// Toggle of notificaion bar.
+const handleToggle = (id) => {
+  var x = document.getElementById(id);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 const Notifications = ({NotifData, PageTitle}) => {
   return ( 
       <div className="outter">
-      <p className="is-size-4 py-5 my-2 is-bold-roman">title</p>
+      <p className="is-size-4 py-5 my-2 is-bold-roman">{PageTitle}</p>
       {NotifData.map((notif, index)=>{
         // Where Notifications are shown as divs.
         return <div key={index}>
@@ -13,20 +24,21 @@ const Notifications = ({NotifData, PageTitle}) => {
               <div className="card-header-title column is-8 has-text-left">
                 {notif.title}
               </div>
-              <button className="column is-1 is-offset-2 card-header-icon" aria-label="more options">
+              <button onClick={() => handleToggle(index)} className="column is-1 is-offset-2 card-header-icon" aria-label="more options">
                   <span className="icon">
                       <i className="fas fa-angle-down" aria-hidden="true"></i>
                   </span>
               </button>
             </div>
           </div>
-            <div id={index} className="card py-1 my-1 notif-border">
+          {/* Description div - toggleable */}
+            <div id={index} style={{"display": "none"}} className="card py-1 my-1 notif-border">
                 <div className="card-content columns is-mobile">
                     <div className="column is-1 mx-3">
                       <button className='button is-normal'>Delete</button>
                     </div>
                     <div className="column is-10">
-                    <div>lorem lorem lorem lorem</div>
+                    <div>{notif.desc}</div>
                     </div>
                 </div>
             </div>
